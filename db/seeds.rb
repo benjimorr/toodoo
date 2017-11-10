a65e2f2fefb282aa8c358c57cdd3b5abb0b9461d
Ben Morrison <benmorrison0@gmail.com>
2017-11-10 18:30:04 -0500

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'Faker'
+
+my_user = User.create!(name: "Test User", email: "test@toodoo.com", password: "password", password_confirmation: "password")
+
+5.times do
+    Todo.create!(
+        title: Faker::Lorem.characters(10),
+        category: Faker::Lorem.characters(10),
+        user: my_user
+    )
+end
+
+todos = Todo.where(user_id: my_user.id)
+
+20.times do
+    Item.create!(
+        name: Faker::Lorem.characters(15),
+        complete: false,
+        todo: todos.sample
+    )
+end
+
+p "Seeds finished"
+p "#{User.count} users created"
+p "#{Todo.count} todo lists created"
+p "#{Item.count} todo list items created"
